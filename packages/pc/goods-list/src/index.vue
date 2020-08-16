30d55659625b9b2be86f7666f4b9e1f14769cafb
pyd <649939004@qq.com>
2020-08-16 17:47:23 +0800

    [新增公共组建静态页面提交]

--- packages/pc/goods-list/src/index.vue
+++ packages/pc/goods-list/src/index.vue
@@ -5,8 +5,8 @@
       <span class="en-title">MORE recommendation</span>
     </el-header>
     <el-main class="goods-main">
-      <div class="goods-item" v-for="item in 4">
-        <div class="recommen"><span class="recommen-text">掌柜推荐</span></div>
+      <div class="goods-item" v-for="item in 8">
+        <div class="recommen">掌柜推荐</div>
         <img class="goods-img" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1572709130,55384343&fm=26&gp=0.jpg" />
         <div class="goods-info">
           <div class="goods-title">品牌名称商品名称品牌名称商品名称 </div>
@@ -15,9 +15,9 @@
               <span class="unit">RMB</span>
               <span class="price">580.00</span>
             </div>
-            <div class="right-promotion">
-              <span class="promotion-price">满150-100</span>
-            </div>
+<!--            <div class="right-promotion">-->
+<!--              <span class="promotion-price">满150-100</span>-->
+<!--            </div>-->
           </div>
         </div>
       </div>
@@ -54,7 +54,7 @@
   }
   .goods-header{
     width:100%;
-    height:4.8125rem;
+    height:86px;
     background:#848F79;
     display :flex;
     flex-direction :column;
@@ -62,56 +62,57 @@
     justify-content :center;
   }
   .cn-title{
-    font-size :1.125rem;
+    font-size :28px;
     color:#fff;
   }
   .en-title{
-    font-size :0.375rem;
+    font-size :12px;
     color:#fff;
-    transform :scale(0.5);
+    transform :scale(0.83);
   }
   .goods-main{
-    width:100%;
+    width:1280px;
     background :#f4f4f4;
-    padding:1.5625rem 0.75rem;
+    padding:40px 0;
     display flex;
     flex-direction: row;
     flex-wrap :wrap;
-    justify-content :space-between;
   }
   .goods-item{
-    width:calc((100% - 0.2rem)/2);
+    width:300px;
     display :flex;
     flex-direction :column;
     position:relative;
-    margin-bottom :0.25rem;
+    margin-bottom :25px;
+    margin-right :26px;
+  }
+  .goods-item:nth-child(4n){
+    margin-right :0;
   }
   .recommen {
-    width: 1.5625rem;
-    height: 1.5625rem;
+    width: 50px;
+    height: 50px;
     background :#A40000;
     display :flex;
     align-items :center;
     justify-content :center;
-    position:absolute;
-    left:0.5rem;
-    top:0.625rem;
-    border-radius :0.125rem;
-  }
-  .recommen-text{
-    font-size :0.75rem;
-    transform :scale(0.65);
+    font-size :18px;
     color:#fff;
+    position:absolute;
+    left:16px;
+    top:20px;
+    border-radius :4px;
     text-align: center;
   }
   .goods-img{
-    width:100%;
+    width:300px;
+    height:300px;
   }
   .goods-info{
     display :flex;
     flex-direction :column;
     background :#EEEEEE;
-    padding:0.625rem 0.5rem;
+    padding:20px 15px;
   }
   .goods-title{
     display: -webkit-box;
@@ -119,9 +120,9 @@
     -webkit-line-clamp: 2;
     word-break: break-all;
     overflow: hidden;
-    font-size:0.75rem;
+    font-size:16px;
     color:#000;
-    margin-bottom :0.625rem;
+    margin-bottom :12px;
   }
   .goods-price-info{
     display :flex;
@@ -135,26 +136,13 @@
     align-items :flex-end;
   }
   .unit{
-    font-size :0.5rem;
+    font-size :14px;
     color:#000;
-    transform :scale(0.67);
-    transform-origin :left;
+    margin-right :10px;
+    margin-bottom :2px;
   }
   .price{
-    font-size :0.875rem;
-    color:#EB0000;
-  }
-  .right-promotion{
-    height:1rem;
-    border-radius :0.5625rem;
-    border:1px solid #EB0000;
-    display:flex;
-    align-items:center;
-    justify-content :center;
-  }
-  .promotion-price{
-    font-size :0.75rem;
-    transform :scale(0.833);
+    font-size :20px;
     color:#EB0000;
   }
 </style>
---
+++ packages/pc/recommen-list/src/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="recommen-list">
+    <el-main class="recommen-main">
+      <img class="left-img" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1572709130,55384343&fm=26&gp=0.jpg"/>
+      <div class="right-content">
+        <div class="goods-item" v-for="item in 4">
+          <img class="goods-img" src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1572709130,55384343&fm=26&gp=0.jpg" />
+          <div class="goods-info">
+            <div class="goods-title">品牌名称商品名称品牌名称商品名称 </div>
+            <div class="goods-price-info">
+              <div class="left-price">
+                <span class="unit">RMB</span>
+                <span class="price">580.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </el-main>
+  </div>
+</template>
+
+<script>
+  import {VueExtend} from 'godspen-lib'
+
+  export default {
+    mixins: [VueExtend.mixin],
+    name: 'recommen-list',
+    label: process.env.LABEL,
+    style: process.env.STYLE,
+    stack: null, // 是否是堆叠模式 ，false：孩子元素会按楼层一个个向下排布， true: 孩子元素绝对定位，随意放置位置
+    childLimit: 9999,  // 孩子元素最大限制数
+    leaf: false, // 是否是叶子节点，为true的时候该节点下面不能添加节点
+    props: {
+    },
+    computed: {
+    },
+    editorMethods: {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" type="text/stylus" scoped>
+  .component {
+    width: 100%;
+    height: 100%;
+  }
+  .recommen-main{
+    width:1280px;
+    border:1px solid rgba(203,203,203,1);
+    box-shadow:0px 0px 12px 0px rgba(0, 0, 0, 0.14);
+    padding:54px;
+    display :flex;
+    flex-direction :row;
+  }
+  .left-img{
+    width:520px;
+    height:848px;
+    margin-right :24px;
+  }
+  .right-content{
+    display :flex;
+    flex-direction :row;
+    flex-wrap :wrap;
+    width:626px;
+  }
+  .goods-item{
+    width:300px;
+    height:408px;
+    display :flex;
+    flex-direction :column;
+    position:relative;
+    margin-bottom :26px;
+    margin-right :26px;
+  }
+  .goods-item:nth-child(2n){
+    margin-right :0;
+  }
+  .goods-img{
+    width:300px;
+    height:300px;
+  }
+  .goods-info{
+    display :flex;
+    flex-direction :column;
+    background :#EEEEEE;
+    padding:20px 15px;
+  }
+  .goods-title{
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    word-break: break-all;
+    overflow: hidden;
+    font-size:16px;
+    color:#000;
+    margin-bottom :12px;
+    height:38px;
+  }
+  .goods-price-info{
+    display :flex;
+    flex-direction :row;
+    justify-content: space-between;
+    align-items :center;
+  }
+  .left-price{
+    display :flex;
+    flex-direction :row;
+    align-items :flex-end;
+  }
+  .unit{
+    font-size :14px;
+    color:#000;
+    margin-right :10px;
+    margin-bottom :2px;
+  }
+  .price{
+    font-size :20px;
+    color:#EB0000;
+  }
+</style>
