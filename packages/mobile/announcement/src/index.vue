e26d944f3eb89478c97f5ed3842a5eadf81b9d94
pyd <649939004@qq.com>
2020-08-23 00:03:44 +0800

    [移动端 公告组建优化]

--- packages/mobile/announcement/src/index.vue
+++ packages/mobile/announcement/src/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="annouceComment">
     <div class="annouce-box">
-      <img class="notice-icon" src="./assets/notice.png"/>
+      <img class="notice-icon" :src="hornIcon"/>
       <span class="notice-label">{{title}}</span>
-      <spna class="notice-content">{{ content }}</spna>
+      <!-- 滚动文字区域 -->
+      <div class="notice-content">
+        <ul id="marquee">
+          <li id="text">{{content}}</li>
+        </ul>
+      </div>
+<!--      <spna class="notice-content">{{ content }}</spna>-->
     </div>
   </div>
 </template>
 
 <script>
   import {VueExtend} from 'godspen-lib'
-
   export default {
     mixins: [VueExtend.mixin],
     name: 'announcement',
@@ -31,11 +36,38 @@
         type: String
       },
     },
+    data: function () {
+      return {
+        hornIcon: 'src/assets/notice.png'
+      }
+    },
     computed: {
     },
     editorMethods: {
     },
+    mounted: function () {
+      // 延时滚动
+      setTimeout(() => {
+        this.move()
+      }, 1000)
+    },
     methods: {
+      move: function () {
+        // 获取内容区宽度
+        let width = document.getElementById('marquee').scrollWidth
+        let marquee = document.getElementById('marquee')
+        let speed = 15 // 位移距离
+        // 设置位移
+        setInterval(function () {
+          speed = speed - 1
+          // 如果位移超过文字宽度，则回到起点
+
+          if (-speed >= width) {
+            speed = document.getElementById('text').offsetWidth
+          }
+          marquee.style.transform = 'translateX(' + speed + 'px)'
+        }, 40)
+      }
     }
   }
 </script>
@@ -69,5 +101,23 @@
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
+  }
+  #box {
+    height: 100%;
+  }
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+  }
+  ul {
+    white-space: nowrap;
+    margin: 0 10px;
+  }
+  li {
+    height: 100%;
+    list-style: none;
+    margin-right: 10px;
+    display: inline-block;
   }
 </style>
