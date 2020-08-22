c7ecd067dffd3748fb76d87a5836e17bb2ddaa40
yangxiaodong <yangxiaodong@qianmi.com>
2020-08-22 14:08:14 +0800

    add;

--- packages/mobile/goods-list/src/index.vue
+++ packages/mobile/goods-list/src/index.vue
@@ -89,11 +89,11 @@
       console.log(this.checklist)
     },
     methods: {
-      onClick ( skuInfo) {
+      onClick( skuInfo) {
         window.parent.postMessage({
           action: 'click',
           payload: skuInfo
-        })
+        }, '*')
       }
     }
   }
