5e4df00a78213d1c9aa4135fd2127f87bd23ac94
yangxiaodong <yangxiaodong@qianmi.com>
2020-08-22 12:51:24 +0800

    add: nav ui 改下. 添加image link 关联添加起来.

--- packages/mobile/catenav/editor/index.vue
+++ packages/mobile/catenav/editor/index.vue
@@ -16,18 +16,26 @@
       <el-collapse v-show="componentInfo.navs && componentInfo.navs.length">
         <el-collapse-item :title="'格子' + (index + 1)"
                           v-for="(item, index) in componentInfo.navs" :key="index">
-          <el-form :model="item" label-width="100px" size="mini">
-            <el-form-item label="图片">
-              <attr-resource type="image" :url.sync="item.img"></attr-resource>
-            </el-form-item>
-            <el-form-item label="跳转">
-              <attr-resource type="text" :url.sync="item.url"></attr-resource>
-            </el-form-item>
-            <el-form-item label="">
-              <el-button type="danger" size='medium'
-                         @click="deleteItem(item, index)">移除当前项</el-button>
-            </el-form-item>
-          </el-form>
+          <el-image
+              style="width: 100px; height: 100px"
+              :src="url"
+              :fit="fit"
+              @click="chooseImage(item, index)"></el-image>
+          <el-link type="primary" @click="chooseLink(item, index)">链接</el-link>
+          <el-link type="primary" @click="deleteItem(item, index)">删除</el-link>
+
+<!--          <el-form :model="item" label-width="100px" size="mini">-->
+<!--            <el-form-item label="图片">-->
+<!--              <attr-resource type="image" :url.sync="item.img"></attr-resource>-->
+<!--            </el-form-item>-->
+<!--            <el-form-item label="跳转">-->
+<!--              <attr-resource type="text" :url.sync="item.url"></attr-resource>-->
+<!--            </el-form-item>-->
+<!--            <el-form-item label="">-->
+<!--              <el-button type="danger" size='medium'-->
+<!--                         @click="deleteItem(item, index)">移除当前项</el-button>-->
+<!--            </el-form-item>-->
+<!--          </el-form>-->
         </el-collapse-item>
       </el-collapse>
     </el-card>
@@ -43,7 +51,6 @@
     },
     data: function () {
       return {
-        usedatasource: false,
       }
     },
     computed: {
@@ -57,16 +64,15 @@
       }
     },
     mounted: function () {
+      if (!this.componentInfo.navs) this.$set(this.componentInfo, 'navs', [])
     },
     methods: {
       // 增加一个格子项
       addItem: function () {
         // 如果不存在 navs 属性，应主动添加响应式属性
-        debugger
-        if (!this.componentInfo.navs) this.$set(this.componentInfo, 'navs', [])
         this.componentInfo.navs.push({
           img: 'http://iph.href.lu/160x40?text=激活&fg=FF0000&bg=CCCCCC',
-          href: '',
+          linkInfo: null,
         })
       },
       // 复制一个格子项
@@ -78,6 +84,28 @@
       // 移除一个格子项
       deleteItem (item, index) {
         this.componentInfo.navs.splice(index, 1)
+      },
+      chooseImage (item, index) {
+        if (window.xExtend && window.xExtend.chooseImage) {
+          window.xExtend.chooseImage({
+            onSubmit: ([image]) => {
+              this.componentInfo.navs[index].img = image
+            }
+          })
+        } else {
+          this.componentInfo.navs[index].img = 'http://iph.href.lu/160x40?text=激活&fg=FF0000&bg=CCCCCC'
+        }
+      },
+      chooseLink (item, index) {
+        if (window.xExtend && window.xExtend.chooseLink) {
+          window.xExtend.chooseLink({
+            onSubmit: ([linkInfo]) => {
+              this.componentInfo.navs[index].linkInfo = linkInfo;
+            }
+          })
+        } else {
+          this.componentInfo.navs[index].linkInfo = {}
+        }
       }
     }
   }
